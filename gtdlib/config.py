0731650e7e82d6d12960a636d7a3b9c30624747f
AB Mostert <104625015+abmostert@users.noreply.github.com>
2026-02-16 13:27:34 +0000

    fix import line from context.py in config.py

--- gtdlib/config.py
+++ gtdlib/config.py
@@ -1,4 +1,5 @@
-from gtdlib.store import ensure_config, normalize_context
+from gtdlib.store import ensure_config
+from gtdlib.rules.context import normalize_context
 
 
 def get_contexts(base_dir):
